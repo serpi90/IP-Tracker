83265881a28fe6619a8a0116208e0ce8f2d4c501
Julian Maestri <serpi90@gmail.com>
2019-11-04 01:17:25 -0300

    fix: undefined index errors

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 header('Content-Type: text/plain');
 function detect_client_ip() {
 	$ip_address = 'UNKNOWN';
-	if ($_SERVER['HTTP_CLIENT_IP']) $ip_address = $_SERVER['HTTP_CLIENT_IP'];
-	else if ($_SERVER['HTTP_X_FORWARDED_FOR']) $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
-	else if ($_SERVER['HTTP_X_FORWARDED']) $ip_address = $_SERVER['HTTP_X_FORWARDED'];
-	else if ($_SERVER['HTTP_FORWARDED_FOR']) $ip_address = $_SERVER['HTTP_FORWARDED_FOR'];
-	else if ($_SERVER['HTTP_FORWARDED']) $ip_address = $_SERVER['HTTP_FORWARDED'];
-	else if ($_SERVER['REMOTE_ADDR']) $ip_address = $_SERVER['REMOTE_ADDR'];
+	if (isset($_SERVER['HTTP_CLIENT_IP'])) $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+	else if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	else if (isset($_SERVER['HTTP_X_FORWARDED'])) $ip_address = $_SERVER['HTTP_X_FORWARDED'];
+	else if (isset($_SERVER['HTTP_FORWARDED_FOR'])) $ip_address = $_SERVER['HTTP_FORWARDED_FOR'];
+	else if (isset($_SERVER['HTTP_FORWARDED'])) $ip_address = $_SERVER['HTTP_FORWARDED'];
+	else if (isset($_SERVER['REMOTE_ADDR'])) $ip_address = $_SERVER['REMOTE_ADDR'];
 	return $ip_address;
 }
 function register_site($db, $site, $ip) {
